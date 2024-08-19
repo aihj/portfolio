dea22793641175815d859c1bb887e604dcb95ac6
aihj <heejae9102@gmail.com>
2024-08-19 11:07:25 +0900

    init

--- src/data/seo.js
+++ src/data/seo.js
@@ -2,14 +2,14 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+			""저는 AI 및 database를 다루어본 경력을 살려서 서비스를 한차원 높게 바라보고, 무엇보다 사용자 눈높이에 맞추어 앱 화면을 구성하고 디자인하는 Frontend 개발자로 성장하고 싶습니다. 저의 장점은 상대적으로 다양한 분야를 경험했기 때문에 폭넓은 시각에서 개발 프로젝트를 접근할 수 있다는 것입니다. 또한 모르는 기술이 있더라도 필요하다면 능동적으로 찾아보고 학습하여 업무에 적극적으로 활용한다는 것입니다.",
 		keywords: ["Heejae Kim"],
 	},
 
 	{
 		page: "about",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+			"저의 장점은 AI부터 swift에 이르기까지 다양한 분야의 개발 경험이 있고, 이를 바탕으로 한 다양한 프로젝트 경험이 있다는 것입니다. 특히 저는 야간대학원에 재학중으로 다양한 domain에서 일하고 있는 현직 개발자들과 소통하고 있기에 유연한 사고를 기르게 되었으며 새로운 도전에 늘 열려있는 자세를 가지고 있습니다.",
 		keywords: ["Heejae Kim"],
 	},
 
